ee2fd0c99d240f767fb5f7118938a6eb6f3b7fcb
宋召立 <song-zhaoli@outlook.com>
2016-01-31 16:37:56 +0800

    解决登录密码框被键盘挡住的bug

---
+++ Huaban.UWP/Views/Content/LoginView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml;
+using Windows.Foundation.Metadata;
+namespace Huaban.UWP.Views
+{
+	public sealed partial class LoginView : UserControl
+	{
+		private Frame Shell;
+		public LoginView()
+		{
+			this.InitializeComponent();
+			this.Loaded += LoginView_Loaded;
+			this.Unloaded += LoginView_Unloaded;
+		}
+
+		private void LoginView_Unloaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+		{
+			Shell.SizeChanged -= Shell_SizeChanged;
+		}
+
+		private void LoginView_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+		{
+			if (ApiInformation.IsTypePresent("Windows.Phone.UI.Input.HardwareButtons"))
+			{
+				rootPanel.Margin = new Thickness(0, 21, 0, 0);
+			}
+			Shell = Window.Current.Content as Frame;
+			this.Width = Shell.ActualWidth;
+			this.Height = Shell.ActualHeight;
+			Shell.SizeChanged += Shell_SizeChanged;
+		}
+
+		private void Shell_SizeChanged(object sender, SizeChangedEventArgs e)
+		{
+			this.Width = Shell.ActualWidth;
+			this.Height = Shell.ActualHeight;
+		}
+	}
+}
