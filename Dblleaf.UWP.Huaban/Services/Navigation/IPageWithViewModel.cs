8b7e18990416bf6fccfd3758c41b22ac04663f1f
zhaso <okokit@126.com>
2018-09-14 21:02:40 +0800

    update

---
+++ Dblleaf.UWP.Huaban/Services/Navigation/IPageWithViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dblleaf.UWP.Huaban.Services.Navigation
+{
+    public interface IPageWithViewModel<T>
+        where T : class
+    {
+        T ViewModel { get; set; }
+
+        void UpdateBindings();
+    }
+}
